41dad882a793257d66e8e9d4f8f01bfcddfcfbad
prafitradimas <prtrdms@gmail.com>
2023-10-26 20:30:29 +0700

    feat: initial commit@front-end

---
+++ consumer-front-end/src/main/java/com/github/prafitradimas/consumer/front/end/ConsumerFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.github.prafitradimas.consumer.front.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFrontEndApplication.class, args);
+	}
+
+}
---
+++ consumer-front-end/src/test/java/com/github/prafitradimas/consumer/front/end/ConsumerFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prafitradimas.consumer.front.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
